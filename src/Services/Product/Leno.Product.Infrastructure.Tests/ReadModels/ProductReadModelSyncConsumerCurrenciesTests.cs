4c94fc70f351fa88aa26400c212583cc75bbe9f7
junjie <1351653168@qq.com>
2026-07-23 01:09:01 +0800

    T9: 修复 ProductReadModelSyncConsumerCurrenciesTests 添加 Product.Domain.ValueObjects using 引用

--- src/Services/Product/Leno.Product.Infrastructure.Tests/ReadModels/ProductReadModelSyncConsumerCurrenciesTests.cs
+++ src/Services/Product/Leno.Product.Infrastructure.Tests/ReadModels/ProductReadModelSyncConsumerCurrenciesTests.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using Leno.Infrastructure.ReadModel;
 using Leno.Product.Domain.Aggregates;
 using Leno.Product.Domain.Repositories;
+using Leno.Product.Domain.ValueObjects;
 using Leno.Product.Infrastructure.ReadModels;
 using Leno.SharedContracts.Events;
 using Leno.SharedKernel.ValueObjects;
