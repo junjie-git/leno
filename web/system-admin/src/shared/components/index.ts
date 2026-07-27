6d3e530b24808eb70f11db553a9e69fa587ae659
junjie <1351653168@qq.com>
2026-07-27 12:50:25 +0000

    feat(system-admin): 添加 shared/components 出口文件

---
+++ web/system-admin/src/shared/components/index.ts
@@ -0,0 +1,16 @@
+/**
+ * shared/components 出口
+ *
+ * 所有业务模块从这里导入共享组件，避免相对路径耦合。
+ */
+export { default as StatusTag } from './StatusTag.vue'
+export { default as IdempotencyButton } from './IdempotencyButton.vue'
+export { default as DataTable } from './DataTable.vue'
+export { default as EmptyState } from './EmptyState.vue'
+export { default as ConfirmDialog } from './ConfirmDialog.vue'
+export { default as DateTimeRangePicker } from './DateTimeRangePicker.vue'
+export { default as JsonViewer } from './JsonViewer.vue'
+export { default as ErrorBoundary } from './ErrorBoundary.vue'
+export { default as ChartLine } from './charts/ChartLine.vue'
+export { default as ChartBar } from './charts/ChartBar.vue'
+export { default as ChartPie } from './charts/ChartPie.vue'
