73d3243ceae86c7e4c11104b6573eba0ec2d3d5e
谭俊杰 <junjie@tanjunjiedeMacBook-Pro.local>
2026-09-24 00:46:09 +0800

    删除临时文件

--- _up2.py
+++
@@ -1,6 +0,0 @@
-out = []
-r = os.popen("docker ps --format {{.Names}}|{{.Status}}").read()
-out.append(r.strip())
-with open("_up2.txt", "w", encoding="utf-8") as f:
-    f.write("\n".join(out))
-print("OK")
